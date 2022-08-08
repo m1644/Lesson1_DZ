c72797767a4a0c89f2b49cc30f0364674a4b298e
MaxX <maxx-f@mail.ru>
2022-08-08 09:55:29 +0300

    Add DZ_001

---
+++ DZ_001/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Результат - ");
+if (numberA > numberB)
+Console.WriteLine("Число А больше числа В");
+else
+{
+    Console.WriteLine("Число А меньше числа В");
+}
